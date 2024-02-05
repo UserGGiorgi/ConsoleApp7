29c87826d1bb8b1207a301b38b527f89b827d3ea
Giorgi <Giorgi@LAPTOP-JPK2IU63>
2024-02-05 20:02:48 +0500

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+
+            Console.WriteLine("Enter Side 1:");
+            triangle.a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Side 2:");
+            triangle.b =Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter Side 3:");
+            triangle.C =Convert.ToInt32(Console.ReadLine());
+
+            if (triangle.a + triangle.b <= triangle.C || triangle.a + triangle.C <= triangle.b || triangle.C + triangle.b <= triangle.a)
+            {
+
+                Console.WriteLine("Triangle Is Not Valid");
+
+            }
+            else
+            {
+                Console.WriteLine("perimeter:" + triangle.perimeter());
+                Console.WriteLine("arrea:" + triangle.area());
+            }             
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp7/Triangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Triangle
+    {
+        public int a { get; set; } 
+        public int b { get; set; }
+        int c;
+        public int C
+        {
+            get { return c; }
+            set
+            {
+                    c = value;
+                
+            }
+         }
+
+        public  int perimeter()
+        {
+            int d = a + b + C;
+            return d ;
+
+        }
+
+        public  double area()
+        {
+            int s = (a + b + C) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - C));
+
+            return area;
+        }
+    }
+}
